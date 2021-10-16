737b4c9c3a04134e6c953ba5d5c7e03b07a4b72d
Cryptopragmatic <tristan.alberio@gmail.com>
2021-10-16 21:42:16 +0800

    black hat first lesson

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect(); 
+    //Returns the arguments that this program was started with (normally passed via the command line)
+    //.collect method put to vector array of strings
+    //env::args() calls the method args from this module
+    
+    println!("{} arguments passed", args.len());
+    
 }
